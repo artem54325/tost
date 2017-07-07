ac83ef590e73a34030f46d332bb98a8c4ea42124
artem54325 <chilo997@mail.ru>
2017-07-08 03:23:42 +0500

    test

--- Desktop/Test1/src/main/java/test/Search.java
+++ Desktop/Test1/src/main/java/test/Search.java
@@ -68,6 +68,7 @@ public class Search {
                 if (bottom.getNumber()>list.get(i).getNumber())
                     continue;
                 Collections.sort(sortList);
+
                 for (int a=0;i<sortList.size();a++){
                     if ((sortList.get(a).getWord().compareTo(list.get(i).getWord())>=0&&sortList.get(a).getNumber()>list.get(i).getNumber())
                             &&(sortList.get(a+1).getWord().compareTo(list.get(i).getWord())<=0&&sortList.get(a+1).getNumber()<list.get(i).getNumber())){
@@ -80,7 +81,7 @@ public class Search {
         return sortList;
     }
 
-    public String parsText(String word){//Ищет подходящие слова, если их не находит возвращает пустой массив.
+    private String parsText(String word){//Ищет подходящие слова, если их не находит возвращает пустой массив.
 //        List<StringIntObj> list = sort(tree.search(word));
         List<StringIntObj> list = (tree.search(word));
         StringBuffer buffer = new StringBuffer();
@@ -88,7 +89,7 @@ public class Search {
             return null;
         Collections.sort(list);
         for (int i = 0;i<10&&i<list.size();i++){
-            buffer.append(list.get(i).get() + " ");
+            buffer.append(list.get(i).get()).append(" ");
         }
         return String.valueOf(buffer);
     }
